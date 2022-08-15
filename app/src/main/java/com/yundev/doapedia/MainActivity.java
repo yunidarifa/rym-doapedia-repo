cbbf9224d2a05d5b0afa5c8c66f631edf4399eac
RIFA YUNIDAR MAARIF <kakanakalmamuchy@gmail.com>
2022-08-15 21:18:31 +0700

    add all files

---
+++ app/src/main/java/com/yundev/doapedia/MainActivity.java
@@ -0,0 +1,22 @@
+package com.yundev.doapedia;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
